c4ca71286bd56de13dd766caa41c2915aab2fbc7
jenniferkerolus <jennifer.kerolus@gmail.com>
2023-03-05 23:56:04 -0500

    HomeWork3

--- src/HomeWork3/Task2.java
+++ src/HomeWork3/Task2.java
@@ -5,20 +5,23 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Task1 {
+import java.util.List;
+
+public class Task2 {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "Driver/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("http://practice.syntaxtechs.net/basic-checkbox-demo.php");
         driver.manage().window().maximize();
-        WebElement checkBoxBtn=driver.findElement(By.cssSelector("input[id='isAgeSelected']"));
-        Thread.sleep(3000);
-        boolean isSelectedBtn=checkBoxBtn.isSelected();
-        if(!isSelectedBtn) {
-            checkBoxBtn.click();
+        List<WebElement> checkBtns=driver.findElements(By.xpath("//input[@class='cb1-element']"));
+        for(WebElement btn:checkBtns){
+            String option=btn.getAttribute("value");
+            if(option.equalsIgnoreCase("option-1")) {
+                btn.click();
+                Thread.sleep(3000);
+                driver.quit();
+
+            }
         }
-        isSelectedBtn=checkBoxBtn.isSelected();
-        Thread.sleep(3000);
-        driver.quit();
     }
 }
