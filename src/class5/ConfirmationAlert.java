bec70da3c5cd76106cc1069b06b09d5946d11145
jenniferkerolus <jennifer.kerolus@gmail.com>
2023-03-14 00:57:42 -0400

    HomeWork4

---
+++ src/class5/ConfirmationAlert.java
@@ -0,0 +1,15 @@
+package class5;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Alert {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "Driver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://practice.syntaxtechs.net/javascript-alert-box-demo.php");
+        driver.findElement(By.xpath(""))
+    }
+}
